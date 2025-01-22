00bc8265da0dbb498814ae897cfa90355945ee2e
Tu Nombre <nathalie200008@gmail.com>
2025-01-22 15:52:40 -0500

    cap 3 - imagenes - agregar lusitana falso

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,14 @@
 import { Inter } from 'next/font/google';
 import { Montserrat } from 'next/font/google';
+
+
 export const inter = Inter({ subsets: ['latin']});
 export const montserrat = Montserrat({
+    weight:['400','700'],
+    subsets: ['latin']
+});
+
+export const lusitana = Montserrat({
     weight:['400','700'],
     subsets: ['latin']
 });
